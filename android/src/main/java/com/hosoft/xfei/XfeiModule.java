f339d65bf468f646c3342e79afc2d6b4dc571c4b
kangqiang <kangqiang@yeteam.com>
2019-06-18 22:48:53 +0800

    feat: support迁移到androidx

--- android/src/main/java/com/hosoft/xfei/XfeiModule.java
+++ android/src/main/java/com/hosoft/xfei/XfeiModule.java
@@ -3,7 +3,7 @@ package com.hosoft.xfei;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
